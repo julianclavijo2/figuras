8211f8831e8952d43f372aab5be9280fe4ac91f8
Julian Clavijo <julianclavijo@Julians-iMac.local>
2022-05-11 20:51:58 -0500

    first

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # curso-practico-javascript
 curso-practico-javascript
+## taller 1:figuras geometricas
+
+-definir las formulas 
+-implementar formulas en js
+-crear funciones 
+-integras js con htmls
---
+++ figuras.js
@@ -0,0 +1,25 @@
+const ladoCuadrado = 5;
+
+console.log('los lados del cuadrado miden : ' + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+
+console.log('perimetro del cuadrado miden : ' + perimetroCuadrado);
+
+
+const areCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log('el area del cuadrado mide : ' + areCuadrado);
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log('los lados del triangulo son :'
++ ladoTriangulo1
++ ladoTriangulo2
++ baseTriangulo
+);
+
+
+
