3d258a89798cd4b49ce79bd3d73ecba4a3083616
Julian Clavijo <julianclavijo@Julians-iMac.local>
2022-05-16 16:29:07 -0500

    commit2

--- figuras.js
+++ figuras.js
@@ -1,5 +1,7 @@
 const ladoCuadrado = 5;
 
+console.group('Cuadrado');
+
 console.log('los lados del cuadrado miden : ' + ladoCuadrado);
 
 const perimetroCuadrado = ladoCuadrado * 4;
@@ -15,16 +17,30 @@ const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
 
+
+console.groupEnd();
+
+console.group('Triangulo');
+
 console.log('los lados del triangulo son :'
-+ ladoTriangulo1
-+ ladoTriangulo2
-+ baseTriangulo
++ ladoTriangulo1 + ' , '
++ ladoTriangulo2 + ' , '
++ baseTriangulo 
 );
 
 const alturatriangulo = 5.5
 console.log('la altura del triangulo  mide : ' + alturatriangulo);
 
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+
+console.log('El perimetro del triangulo mide : ' + perimetroTriangulo);
+
 const areaTriangulo = (baseTriangulo * alturatriangulo) / 2;
+
+console.log('El area del triangulo mide : ' + areaTriangulo);
+
+
+console.groupEnd();
 
 
 
