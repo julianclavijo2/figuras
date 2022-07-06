f7641490fe5766e535ac5247e4a6daf12e29092e
Julian Clavijo <julianclavijo@Julians-iMac.local>
2022-07-06 17:57:53 -0500

    update circulo

--- figuras.js
+++ figuras.js
@@ -25,7 +25,7 @@ const operationSquare = (opt) => {
 
             let h1Perimetro = document.createElement('h1');
             h1Perimetro.innerHTML = 'El perimetro del cuadrado es:';
-            let h1ResultPerimetro = document.createElement('h1');
+            let h1ResultPerimetro = document.createElement('h1');   
             h1ResultPerimetro.innerHTML = perimetroCuadrado;
             h1ResultPerimetro.style.color = '#5f9ea0';
 
@@ -87,18 +87,18 @@ const operationTriangulo = (opt) => {
 }
 
 const operationCirculo = (opt) => {
-    const alturaTriangulo = document.getElementById('alturaTriangulo');
-    const baseTriangulo = document.getElementById('baseTriangulo');
+    const diametro = document.getElementById('diametroCirculo');
+    const radio = document.getElementById('radioCirculo');
     document.getElementsByClassName('body-content')[0].replaceChildren();
     switch (opt) {
         case 1:
-            let areaTriangulo = alturaTriangulo.value * baseTriangulo.value / 2;
+            let radioCirculo = diametro.value  / 2;
             document.getElementById('popup').style.display = 'block';
 
             let h1 = document.createElement('h1');
-            h1.innerHTML = 'El area es del triangulo es:';
+            h1.innerHTML = 'El radio del circulo es:';
             let h1Result = document.createElement('h1');
-            h1Result.innerHTML = areaTriangulo;
+            h1Result.innerHTML = radioCirculo;
             h1Result.style.color = '#5f9ea0';
 
             document.getElementsByClassName('body-content')[0].append(h1);
@@ -107,19 +107,18 @@ const operationCirculo = (opt) => {
 
             break;
         case 2:
-            const perimetroTriangulo = baseTriangulo.value * 3;
-            
 
+            const areaCirculo = ((Math.PI) * (radio.value * radio.value));
             document.getElementById('popup').style.display = 'block';
 
             let h1Perimetro = document.createElement('h1');
-            h1Perimetro.innerHTML = 'El perimetro del triangulo es:';
-            let ResultPerimetro = document.createElement('h1');
-            ResultPerimetro.innerHTML = perimetroTriangulo;
-            ResultPerimetro.style.color = '#5f9ea0';
+            h1Perimetro.innerHTML = 'El area del circulo es:';
+            let ResultArea = document.createElement('h1');
+            ResultArea.innerHTML = areaCirculo;
+            ResultArea.style.color = '#5f9ea0';
 
             document.getElementsByClassName('body-content')[0].append(h1Perimetro);
-            document.getElementsByClassName('body-content')[0].append(ResultPerimetro);
+            document.getElementsByClassName('body-content')[0].append(ResultArea);
 
             break;
 
@@ -133,70 +132,5 @@ const cerrar = () => {
     popup.style.display = 'none';
 }
 
-
-/* const ladoCuadrado = 5;
-
-console.group('Cuadrado');
-
-console.log('los lados del cuadrado miden : ' + ladoCuadrado);
-
-
-
-console.log('perimetro del cuadrado miden : ' + perimetroCuadrado);
-
-
-const areCuadrado = ladoCuadrado * ladoCuadrado;
-
-console.log('el area del cuadrado mide : ' + areCuadrado);
-
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-
-
-console.groupEnd();
-
-console.group('Triangulo');
-
-console.log('los lados del triangulo son :'
-+ ladoTriangulo1 + ' , '
-+ ladoTriangulo2 + ' , '
-+ baseTriangulo 
-);
-
-const alturatriangulo = 5.5
-console.log('la altura del triangulo  mide : ' + alturatriangulo);
-
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-
-console.log('El perimetro del triangulo mide : ' + perimetroTriangulo);
-
-const areaTriangulo = (baseTriangulo * alturatriangulo) / 2;
-
-console.log('El area del triangulo mide : ' + areaTriangulo);
-
-
-console.group("Circulos");
-const radioCirculo = 4;
-const diametroCirculo = radioCirculo * 2;
-const PI = Math.PI;
-
-const perimetroCirculo = diametroCirculo * PI;
-const areaCirculo = (radioCirculo * radioCirculo) * PI;
-
-console.log("El radio del circulo es : " + radioCirculo + "cm");
-
-console.log("El diametro del circulo es : " + diametroCirculo + "cm");
-
-console.log('PI es :' + PI + "cm");
-
-console.log("El area del circulo es : " + areaCirculo + "cm");
-
-console.log("El perimetro del circulo es : " + perimetroCirculo + "cm");
-
-
-
-console.groupEnd();
- */
 
 
